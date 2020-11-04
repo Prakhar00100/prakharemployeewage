492aae5186724ab5d7a5e50b28c5209bdf217345
prakhar00100 <prakhar00100@gmail.com>
2020-11-04 12:19:25 -0800

    parttimewage

--- emplwage.sh
+++ emplwage.sh
@@ -1,21 +1,25 @@
 public class EmployeeWage {
 
    public static void main(String[] args) {
-
-      int IS_FULL_TIME = 1;
+      int IS_PART_TIME = 1;
+      int IS_FULL_TIME = 2;
       int EMP_RATE_PER_HOUR = 20;
       int empHrs = 0;
       int empWage = 0;
-      double empCheck = Math.floor(Math.random() * 10) % 2;
+      double empCheck = Math.floor(Math.random() * 10) % 3;
 
-      if (empCheck == IS_FULL_TIME) {
+      if (empCheck == IS_PART_TIME) {
+         System.out.println("Present");
+          empHrs = 4;
+         }
+      else if (empCheck == IS_FULL_TIME) {
           System.out.println("Present");
           empHrs = 8;
-      }
+         }
       else {
           System.out.println("Absent");
           empHrs = 0;
-           }
+         }
        empWage = empHrs * EMP_RATE_PER_HOUR;
        System.out.println("Emp Wage : " + empWage);
     }
